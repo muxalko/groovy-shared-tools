d5df191c6759c81f4e965db84d4c7a71fb747c29
muxalko <muxa.baba@gmail.com>
2020-12-10 00:48:47 +0200

    Create uploadFile.groovy

---
+++ uploadFile.groovy
@@ -0,0 +1,20 @@
+def inputGetFile(String savedfile = null) {
+    def filedata = null
+    def filename = null
+    
+    if (savedfile == null) {
+        def inputFile = input message: 'Upload file', parameters: [file(name: 'library_data_upload'), string(name: 'filename', defaultValue: 'public.key')]
+        filedata = inputFile['library_data_upload']
+        filename = inputFile['filename']
+    } else {
+        def inputFile = input message: 'Upload file', parameters: [file(name: 'library_data_upload')]
+        filedata = inputFile
+        filename = savedfile
+    }
+
+    // Read contents and write to workspace
+    writeFile(file: filename, encoding: 'Base64', text: filedata.read().getBytes().encodeBase64().toString())
+
+    filedata.delete()
+    return filename
+}
